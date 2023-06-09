13ce9216761d5065cebdc471580075cdb8887059
Júlio Cézar <captaingamergroot@gmail.com>
2023-06-09 13:32:16 -0300

    🚀 Finalizado

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,128 @@
+import BackgroundImg from '@assets/background.png';
+import LogoSvg from '@assets/logo.svg';
+import { Button } from "@components/Button";
+import { Input } from "@components/Input";
+import { useAuth } from '@hooks/useAuth';
+import { useNavigation } from '@react-navigation/native';
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
+import { AppError } from '@utils/AppError';
+import { Center, Heading, Image, ScrollView, Text, VStack, useToast } from "native-base";
+import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+
+type FormData = {
+   email: string;
+   password: string;
+}
+
+export function SignIn() {
+
+   const [isLoading, setIsLoading] = useState(false)
+
+   const { singIn } = useAuth();
+   const toast = useToast()
+   const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+   const { control, handleSubmit, formState: { errors } } = useForm<FormData>()
+
+   function handleNewAccount() {
+      navigation.navigate('signUp');
+   }
+
+
+   async function handleSignIn({ email, password }: FormData) {
+      try {
+         setIsLoading(true)
+         await singIn(email, password);
+      } catch (error) {
+         const isAppError = error instanceof AppError;
+
+         const title = isAppError ? error.message : 'Não foi possível entrar. Tente novamente mais tarde.'
+
+         toast.show({
+            title,
+            placement: 'top',
+            bgColor: 'red.500'
+         })
+         setIsLoading(false)
+      }
+   }
+
+   return (
+      <ScrollView
+         contentContainerStyle={{ flexGrow: 1 }}
+         showsVerticalScrollIndicator={false}
+      >
+         <VStack flex={1} px={10} pb={16} >
+            <Image
+               source={BackgroundImg}
+               defaultSource={BackgroundImg}
+               alt="Pessoas treinando"
+               resizeMode="contain"
+               position='absolute'
+            />
+
+            <Center my={32}>
+               <LogoSvg />
+
+               <Text color="gray.100" fontSize="sm">
+                  Treine sua mente e o seu corpo.
+               </Text>
+            </Center>
+
+            <Center>
+               <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+                  Acesse a conta
+               </Heading>
+
+               <Controller
+                  control={control}
+                  name="email"
+                  rules={{ required: 'Informe o e-mail' }}
+                  render={({ field: { onChange } }) => (
+                     <Input
+                        placeholder="E-mail"
+                        keyboardType="email-address"
+                        autoCapitalize="none"
+                        onChangeText={onChange}
+                        errorMessage={errors.email?.message}
+                     />
+                  )}
+               />
+
+               <Controller
+                  control={control}
+                  name="password"
+                  rules={{ required: 'Informe a senha' }}
+                  render={({ field: { onChange } }) => (
+                     <Input
+                        placeholder="Senha"
+                        secureTextEntry
+                        onChangeText={onChange}
+                        errorMessage={errors.password?.message}
+                     />
+                  )}
+               />
+
+               <Button
+                  title="Acessar"
+                  onPress={handleSubmit(handleSignIn)}
+                  isLoading={isLoading}
+               />
+            </Center>
+
+            <Center mt={48}>
+               <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
+                  Ainda não tem acesso?
+               </Text>
+            </Center>
+
+            <Button
+               title="Criar Conta"
+               variant="outline"
+               onPress={handleNewAccount}
+            />
+         </VStack>
+      </ScrollView>
+   )
+}
